234358bb0b2367d9d1591221e4f2c61726d31619
Peter Tang <petersktang@gmail.com>
2019-08-22 06:26:10 +0800

    initial IGListKit support added

--- RealmIGListKit/SectionController.swift
+++ RealmIGListKit/SectionController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 import IGListKit
+import RealmSwift
 
 class SectionController: ListSectionController {
+    private weak var collectionView: UICollectionView!
+    private let grid: Grid
     
+    private var realmSection: RealmSection?
+    
+    init(grid: Grid, _ collectionView: UICollectionView) {
+        self.grid = grid
+        self.collectionView = collectionView
+    }
+    override func didUpdate(to object: Any) {
+        precondition(object is RealmSection)
+        guard let object = object as? RealmSection else {return}
+        realmSection = object
+    }
+    override func canMoveItem(at index: Int) -> Bool {
+        return true
+    }
+    override func numberOfItems() -> Int {
+        return 0
+    }
+    override func sizeForItem(at index: Int) -> CGSize {
+        return grid.size(for: collectionView, ratio: 1)
+    }
 }
+
+
--- RealmIGListKit/ViewController.swift
+++ RealmIGListKit/ViewController.swift
@@ -39,18 +39,35 @@ class ViewController: UICollectionViewController {
         
         collectionView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.safeAreaLayoutGuide.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
     }
-
 }
 
 extension ViewController: ListAdapterDataSource {
     func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
-        return []
+        return [RealmSection(id: 0), RealmSection(id: 1)]
     }
     func emptyView(for listAdapter: ListAdapter) -> UIView? {
         return nil
     }
     func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
-        return SectionController()
+        return SectionController(grid: grid, collectionView)
     }
 }
 
+final class RealmSection {
+    let sectionId : Int
+    init(id sectionId:Int) {
+        self.sectionId = sectionId
+    }
+}
+
+extension RealmSection: ListDiffable {
+    func diffIdentifier() -> NSObjectProtocol {
+        return sectionId as NSObjectProtocol
+    }
+    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        guard let object = object as? RealmSection else {
+            return false
+        }
+        return self.sectionId == object.sectionId
+    }
+}
